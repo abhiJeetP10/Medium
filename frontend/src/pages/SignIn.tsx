ae6c695d7cd5f3d6398281904491b1296887ffda
Abhijeet <pabhijeet349@gmail.com>
2025-01-01 22:22:09 +0530

    Minor FE changes

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,7 +1,19 @@
 import { Quote } from "../components/Quote";
 import { Auth } from "../components/Auth";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function SignIn() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      navigate("/blogs");
+    }
+  }, []);
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2">
       <div>
